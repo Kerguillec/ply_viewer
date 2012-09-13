86d712a2ece360d969f29bc1aa3142c75845967b
Paul Kerguillec <paul.kerguillec@iutbeziers.fr>
2012-09-13 14:30:48 +0200

    comments added

---
+++ src/model.c
@@ -0,0 +1,11 @@
+/********************************
+
+Entry :
+- Model.c is wrapping all the structures of Point, Triangle and Matrix
+
+
+Exit :
+- Will give the modelisation of your object, for example bunny.ply
+
+
+********************************/
