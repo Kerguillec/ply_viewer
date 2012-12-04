1454c636fee63460665612b9e5490ac9b72bf1ae
Sebastien Druon <seb@homedruon.com>
2012-12-04 17:46:16 +0100

    wrote main for step 3 validation

--- src/main.c
+++ src/main.c
@@ -7,68 +7,17 @@
 #include "model.h"
 int main ( int argc, char** argv, char** envv ) {
  
-	ply_load(argv[1]);
-/*      Tpoint* points1;
-	Tpoint* points2;
-	Tpoint* points3;
-	
-	points1 = point_malloc(1,1,1);
-	point_print(points1, stderr);	
-	
-	points2 = point_malloc(2,2,2);
-	point_print(points2, stderr);	
+	Trigidobject*	lapin ;
 
-	points3 = point_malloc(5,3,2);
-	point_print(points3, stderr);	
-	
-	points3 = point_mallocRGB(5,3,2,64,246,247);
-	point_print(points3, stderr);	
+	printf(" File : %s \n", argv[1] ) ;
 
-	Ttriangle* triangle;
-	Ttriangle* triangle1;
-	
-	triangle = triangle_malloc(points1, points2, points3);
-	triangle_print(triangle,stderr);
-	
-	// With coloration: 
-	triangle1 = triangle_mallocRGB(points1, points2, points3, 54, 23, 245);
-	triangle_print(triangle1,stderr);
-*/
-/*	
-	Tmodel* test;
-	
-	test = model_malloc() ;
-	
-	int i;	
-	// How To use model_add_point & model_add_triangle:
-	for(i=0; i<10; i++)
-	{
-		model_add_point(test, 1, 2, 3);
-		model_add_point(test, 654, 12, 13);
-		model_add_point(test, 11, 521, 81);
-	}
+	lapin = rigidobject_malloc() ;
 
-	for(i=0; i<10; i++)
-		model_add_triangle(test, test->tab_points[0], test->tab_points[1], test->tab_points[2]);	
-		
-	// Set the matrix position of Tmodel* test	
-	Tmatrix* matrix;
-	matrix=matrix_malloc(5.0,4.0,3.0,2.0,
-						1.0,5.0,3.0,2.0,
-						4.0,5.0,6.0,7.0,
-						8.0,9.0,2.0,1.0) ;
-						
-	model_set_posmat(test,matrix);	
-		
-		
-*/		
-/*	
-	point_free(points1);
-	point_free(points2);
-	point_free(points3);
+	rigidobject_loadply( lapin, argv[1] ) ;
 
-	triangle_free(triangle);
-*/
+	rigidobject_print ( lapin ) ;
+
+	rigidobject_free ( lapin ) ;
 
 	return 0 ;
 }
